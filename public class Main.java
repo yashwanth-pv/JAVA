48264229b30325d1bd71d798b2d24d480a389fae
Yashwanth Yadav P V <20yashwanthyadav@gmail.com>
2026-08-20 23:51:22 +0530

    Add files via upload

---
+++ public class Main.java
@@ -0,0 +1,17 @@
+
+class Hello_world {
+
+    public static void main(String[] args) 
+    {
+        System.out.println('a');
+        System.out.println("Yashwanth");
+        System.out.println(45);
+
+        int a = 10;
+        int b = 20;
+        System.out.println("Sum is"+ (a+b));
+
+        boolean result = true;
+        System.out.println("The result is " + result);
+    }
+}
