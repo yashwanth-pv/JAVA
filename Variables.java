732003b95b2be5b0425c67736cee792d7653b31c
Yashwanth Yadav P V <20yashwanthyadav@gmail.com>
2026-09-01 22:09:24 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,30 @@
+# variables are containers used to store data values in memory during program execution. Every variable is assigned a specific data type, which determines the type and size of the values it can hold
+
+Class Hello_world
+  {
+   Public static void main(string[]args)
+  {
+    System.out.println(34);
+    System.out.println('Y');
+    System.out.println("Yashwanth");
+    System.out.println(9.12);
+    System.out.println(true);
+  }
+}
+
+# concatination :- (joining)
+
+  Class Addition
+  {
+   Public static void main(string[]args)
+  {
+    int a = 10;
+    int b = 20;
+    System.out.println("Sum is" + (a+b));
+
+    boolean result = true;
+    System.out.println("The result is " + result);
+  }
+}
+
+# The Java Virtual Machine (JVM) is a software engine that loads, verifies, and executes Java bytecode, turning it into instructions your computer hardware can understand.
