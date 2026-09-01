e1c93489e644dbb9a9fbeb00344e61e834004ba9
Yashwanth Yadav P V <20yashwanthyadav@gmail.com>
2026-09-01 22:22:08 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,21 @@
+# Local Variable
+public class LocalVariable {
+
+    public static void main(String[] args) {
+
+        int number = 10;
+
+        System.out.println("Local variable: " + number);
+    }
+}
+
+# global Variable
+  public class GlobalVariable {
+
+    static int number = 20;
+
+    public static void main(String[] args) {
+
+        System.out.println("Global variable: " + number);
+    }
+}
