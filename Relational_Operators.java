49f003534ce6562b3f073ac02424e451209a1f96
Yashwanth Yadav P V <20yashwanthyadav@gmail.com>
2026-09-02 00:09:30 +0530

    Create Relational_Operators.java

---
+++ Relational_Operators.java
@@ -0,0 +1,14 @@
+class Relational_operators 
+{
+  public static void main(String[] args) 
+  {
+    int x = 5;
+    int y = 3;
+    System.out.println(x == y);
+    System.out.println(x > y);
+    System.out.println(x < y);
+    System.out.println(x != y);
+    System.out.println(x <= y);
+    System.out.println(x >= y);
+  }
+}
