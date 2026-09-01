a4e91b5662e43dee47a5dfeff7968d2e826de546
Yashwanth Yadav P V <20yashwanthyadav@gmail.com>
2026-09-01 22:11:09 +0530

    Update Literals.java

--- Literals.java
+++ Literals.java
@@ -1,19 +1,3 @@
-/*
-Definition
-
-**Literals in Java are fixed constant values written directly in a program. They do not change during program execution and include integer, floating-point, character, string, boolean, and null literals.
-In Java, **literals** are fixed values written directly in the source code. They represent constant values assigned to variables or used in expressions.
-
-| Type               | Example              | Description                    |
-| ------------------ | -------------------- | ------------------------------ |
-| **Integer**        | `100`, `-25`, `0`    | Whole numbers                  |
-| **Floating-point** | `3.14`, `2.5f`       | Decimal numbers                |
-| **Character**      | `'A'`, `'7'`, `'\n'` | A single character             |
-| **String**         | `"Hello"`            | A sequence of characters       |
-| **Boolean**        | `true`, `false`      | Represents true/false          |
-| **Null**           | `null`               | Represents no object reference |
-*/
-
 int x = 100;
 double pi = 3.14;
 char grade = 'A';
